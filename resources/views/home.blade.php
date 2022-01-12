56fdd73abef88e220a1556fbc2f2a2af5a7a0a2e
嶋末麻衣 <shimasuemai@outlook.jp>
2022-01-12 11:33:11 +0900

    修正2022.1.12

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,43 +4,87 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 <title>Laravel mypage</title>
-
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
 <!-- Styles -->
 <link rel="stylesheet" href="{{ asset('css/all.css') }}">
+<link rel="stylesheet" href="{{ asset('css/user.css') }}">
+<style>
+    body{
+        background-color:#222121; 
+    }
+    .menu_bar{
+        margin-top:100px;
+    }
+    .kensaku_textbox{
+    background-color: transparent;
+    border:1px solid #222121; ;
+    color: #fff;
+    cursor: pointer;
+    outline: none;
+    padding: 5px;
+    appearance: none;
+    border:1px #c3d825 solid;
+    width: 309px;
+    height: 50px;
+    font-size: 18px;
+}
+.kensaku_button{
+    background-color:#222121; 
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    padding: 5px;
+    appearance: none;
+    border:1px #c3d825 solid;
+    width: 320px;
+    height: 50px;
+</style>
+</head>
+<body>
+<div class="openbtn"><span></span><span></span><span></span></div>
+<nav id="g-nav">
+    <table width=100% class="menu_bar">
+        <tr>
+            <td>
+                <form method="GET" action="{{ route('Suredo.kensakugo') }}">
+                    <input type="text" class="kenseku_textbox" name="word" required autofocus><br>
+                    <input type="submit" class="kensaku_button" value="スレッド検索">
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="{{ route('Suredo.sakusei') }}">新規記事作成</a>  
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="{{ route('Image.input') }}">マイページ</a>   
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="{{ route('Image.input') }}">概要</a>   
+            </td>
+    </table>   
+</nav>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<!--自作のJS-->
+<script>
+    $(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
 
-<table class="center" width="80%" height="70%">
-   <tr>
-   <td>
-    <table class="kensaku">
-        <tr><td><a href="{{ route('Suredo.sakusei') }}" class="toptext"><img src="../image/hane.png" class="image" alt="作成・編集・削除"></a></td></tr>
-    </table>
-    </td>
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+</script>
 
-    <td>
-    <table class="kensaku">
-        <tr><td><form method="GET" action="{{ route('Suredo.kensakugo') }}">
-                <input type="text" class="textblack" name="word" placeholder="word" required autofocus><br>
-                <input type="submit" class="blackbutton" value="スレッド検索">
-                </form></td></tr>
-    </table>
-    </td>
-    
-   <td>
-    <table class="kensaku">
-    <form method="GET" action="{{ route('Image.input') }}">
-        <tr><td><input type="hidden" name="name" value="{{ Auth::user()->name}}">
-                <input type="image" src="../image/hosi.png" class="image">
-                </form></td></tr>
-    </table>
-    </td>
-
-    </tr>
-</table>
-
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -55,9 +99,11 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
 
-<header class="line">
+<!-- <header class="line">
   今月会員数 : {{$user}} 今月投稿数 : {{$suredo}} : WELCOME {{ Auth::user()->name}}さん</p>
-</header>
+</header> -->
+
+</body>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,10 +38,7 @@
         
             </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a id="podisyon" href="{{ route('logout') }}" class="whitebutton" onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                    ログアウト
-                </a>
+ 
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
